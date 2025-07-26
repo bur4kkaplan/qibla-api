c572476c219f4f710f67695db6ce64050f02825f
bur4kkaplan <108868972+bur4kkaplan@users.noreply.github.com>
2025-07-26 11:16:30 +0300

    app.py

--- app.py
+++ app.py
@@ -58,4 +58,6 @@ def qibla():
     })
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Railway'de 8080 olur
+    app.run(host="0.0.0.0", port=port)
